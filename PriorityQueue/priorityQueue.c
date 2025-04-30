ec1f49e10381e42b6cc91bf31409b28ac2182101
João Víctor Paiva Nunes Pereira <joaov.pnp@gmail.com>
2025-04-30 14:47:02 -0300

    methods included and createQueue() updated

---
+++ PriorityQueue/priorityQueue.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "priorityQueue.h"
+
+priorityQueue *createQueue(const size_t size, priorityRuler ruler) {
+
+    priorityQueue *q = malloc(sizeof(priorityQueue));
+    q->size = size;
+    q->ruler = ruler;
+    q->front = NULL;
+    q->tale = NULL;
+
+    return q;
+}
+
+void destroyQueue(priorityQueue *q) {
+
+    if (q == NULL || isEmpty(q))
+        return;
+    
+    node *aux = q->front;
+    while (aux != NULL) {
+        q->front = aux->next;
+        free(aux->data);
+        free(aux);
+        aux = q->front;
+    }
+
+    free(q);
+}
+
+void enqueue(const T* value, priorityQueue *q) {
+
+    if (q == NULL)
+        return;
+
+    node *newNode = malloc(sizeof(node));
+    newNode->data = malloc(q->size);
+    newNode->next = NULL;
+    memcpy(newNode->data, value, q->size);
+
+    if (q->tale == NULL) {
+        q->front = newNode;
+        q->tale = newNode;
+    } else {
+        q->tale->next = newNode;
+        q->tale = newNode;
+    }
+
+}
+
+T* dequeue(priorityQueue *q) {
+
+    if (q == NULL || isEmpty(q))
+        return NULL;
+
+    T* value = malloc(q->size);
+    memcpy(value, q->front->data, q->size);
+    
+    node *aux = q->front;
+    q->front = aux->next;
+
+    if (aux == q->tale)
+        q->tale = NULL;
+
+    free(aux);
+
+    return value;
+}
+
+T* peek(const priorityQueue *q) {
+
+    if (q == NULL || isEmpty(q))
+        return NULL;
+
+    T* value = malloc(q->size);
+    memcpy(value, q->front->data, q->size);
+
+    return value;
+}
+
+int isEmpty(const priorityQueue *q) {
+    return q == NULL || q->front == NULL;
+}
